fcba67e912a7b7434012f67f5241d5baca129b60
Christian Ezeani <christian.ezeani@gmail.com>
2018-01-30 00:14:36 +0100

    Theme settings modified

--- theme/theme-settings.php
+++ theme/theme-settings.php
@@ -1,21 +1,24 @@
 <?php
 
 function opencharity_form_system_theme_settings_alter(&$form, $form_state) {
-  
-  $form['opencharity_banner'] = array(
-    '#type'          => 'file',
-    '#title'         => t('Banner Image'),
-    '#default_value' => theme_get_setting('opencharity_banner'),
-    '#description'   => t("Changes the banner image on the homepage."),
+  $form['opencharity'] = array(
+    '#type'          => 'fieldset',
+    '#title'         => t('OpenCharity Theme Settings'),
     '#weight' => -2
   );
 
-  $form['opencharity_join_us'] = array(
+  $form['opencharity']['opencharity_banner'] = array(
+    '#type'          => 'file',
+    '#title'         => t('Banner Image'),
+    '#default_value' => theme_get_setting('opencharity_banner'),
+    '#description'   => t("Changes the banner image on the homepage.")
+  );
+
+  $form['opencharity']['opencharity_join_us'] = array(
     '#type'          => 'textfield',
     '#title'         => t('Join Us Link'),
     '#default_value' => theme_get_setting('opencharity_join_us'),
-    '#description'   => t("Sets the \"Join Us\" link on the header"),
-    '#weight' => -2
+    '#description'   => t("Sets the \"Join Us\" link on the header")
   );
 
 }
